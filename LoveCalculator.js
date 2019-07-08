d6652a35728194b0b34d09f3d8d8f78a670153fc
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-07-08 14:23:44 -0400

    "creating love calculator"

---
+++ LoveCalculator.js
@@ -0,0 +1,12 @@
+var nameOne = prompt("What is your name "); 
+var nameTwo = prompt("What is his name");
+
+
+var n = Math.random();
+n = n * 100;
+n = Math.floor(n) + 1;
+console.log(n);
+
+alert("Yout love percentage is " + n + "%");
+
+
